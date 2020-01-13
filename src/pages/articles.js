b5c1bee497ccf34675c49570d84f529bcbfb6c50
Andrew Gilliland <andrewpgilliland@gmail.com>
2020-01-13 12:00:12 -0600

    add articles page

---
+++ src/pages/articles.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ArticlesPage = () => (
+  <Layout>
+    <SEO title="Articles" />
+        <h1>Articles</h1>
+  </Layout>
+)
+
+export default ArticlesPage
