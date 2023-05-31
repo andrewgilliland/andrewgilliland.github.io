098fa9c5a3f05321ff501b38163e3628b9a0ebce
Andrew Gilliland <andrew.gilliland@ppi-twmt2pvx36.prp.local>
2023-05-31 08:34:39 -0500

    feat: styles header-title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,12 +8,24 @@ export default function Header() {
     { href: "/about", title: "About" },
   ];
 
+  const pink300 = "#f9a8d4";
+  const emerald300 = "#6ee7b7";
+  const cyan300 = "#67e8f9";
+  const yellow300 = "#fde047";
+
   return (
     <>
       <div className="flex justify-between items-center border-b-2 border-white-300 px-[10%] py-6">
         <div className="flex items-center">
           <Link href="/">
-            <h1 className="text-xl text-pink-100">Andrew Gilliland</h1>
+            <h1
+              className="text-3xl header-title"
+              style={{
+                background: `linear-gradient( to bottom right, ${pink300}, ${pink300} 30%, ${emerald300} 30%, ${emerald300} 50%, ${yellow300} 50%, ${yellow300} 70%, ${cyan300} 70%, ${cyan300})`,
+              }}
+            >
+              Andrew Gilliland
+            </h1>
           </Link>
         </div>
 
