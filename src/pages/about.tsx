791267d2878b5d019bed286fdaecf03c0e8fdd44
Andrew Gilliland <agilliland@precisionplanting.com>
2023-07-25 08:53:36 -0500

    feat: makes blog and about pages layout responsive

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,7 +3,9 @@ import GameBoy from "@/components/GameBoy";
 const About = () => {
   return (
     <div className="max-w-3xl w-full mx-auto mt-14">
-      <h1 className="font-bold text-gray-100 text-4xl stroke-white">About</h1>
+      <h1 className="font-bold text-gray-100 text-4xl stroke-white mx-8 md:mx-0">
+        About
+      </h1>
 
       {/* <GameBoy /> */}
     </div>
