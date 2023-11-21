e925b31713d6d0bc534245e03afc305178c366b9
Andrew Gilliland <andrewpgilliland@gmail.com>
2023-11-21 08:44:12 -0600

    feat: adds social linkes to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
+import GitHubIcon from "./svg/GitHubIcon";
+import LinkedInIcon from "./svg/LinkedInIcon";
+import TwitterIcon from "./svg/TwitterIcon";
+
 const Footer = () => {
   return (
-    <footer className="flex justify-center px-[10%] py-6">
-      <div>ⓒ {new Date().getFullYear()} Andrew Gilliland </div>
+    <footer className="flex flex-col justify-center items-center px-[10%] py-8">
+      <div>ⓒ {new Date().getFullYear()} Andrew Gilliland</div>
+      <div className="flex items-center gap-10 mt-6">
+        <a href="https://github.com/andrewgilliland">
+          <GitHubIcon className="fill-gray-300 h-5 w-5 transform transition hover:scale-110" />
+        </a>
+        <a href="https://www.linkedin.com/in/andrew-gilliland-a89b8919a/">
+          <LinkedInIcon className="fill-gray-300 h-5 w-5 transform transition hover:scale-110" />
+        </a>
+        <a href="https://twitter.com/droidgilliland">
+          <TwitterIcon className="fill-gray-300 h-5 w-5 transform transition hover:scale-110" />
+        </a>
+      </div>
     </footer>
   );
 };
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,3 @@
-import GitHubIcon from "@/components/svg/GitHubIcon";
-import LinkedInIcon from "@/components/svg/LinkedInIcon";
-import TwitterIcon from "@/components/svg/TwitterIcon";
-
 const About = () => (
   <div className="max-w-3xl w-full mx-auto mt-14 px-8">
     <h1 className="font-bold text-gray-100 text-4xl stroke-white md:mx-0">
@@ -16,12 +12,6 @@ const About = () => (
         technologies. In my spare time, I enjoy learning about fitness and
         nutrition, woodworking and am a music aficionado.
       </p>
-    </div>
-
-    <div className="flex items-center gap-10 mt-12">
-      <GitHubIcon className="fill-gray-300 h-8 w-8 transform transition hover:scale-105" />
-      <LinkedInIcon className="fill-gray-300 h-8 w-8 transform transition hover:scale-105" />
-      <TwitterIcon className="fill-gray-300 h-8 w-8 transform transition hover:scale-105" />
     </div>
   </div>
 );
