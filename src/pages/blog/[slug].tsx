a76420ae7fc01fbabdfaed23ec7c0de2cb0045ab
Andrew Gilliland <andrewpgilliland@gmail.com>
2023-12-05 08:34:40 -0600

    feat: adds blog post and updates prose styles

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -56,10 +56,10 @@ const PostPage = ({ frontmatter: { title, excerpt, date }, slug, content }) => {
         />
       </div>
 
-      <BlogOutlineCard headingElements={headingElements} title={title} />
+      {/* <BlogOutlineCard headingElements={headingElements} title={title} /> */}
 
       <div
-        className="prose prose-h2:text-yellow-300 prose-h3:text-gray-300 prose-h4:text-gray-300 prose-h4:ml-4 prose-h5:text-gray-500 prose-p:text-gray-200 prose-ul:text-gray-200 prose-pre:border-2 prose-pre:border-gray-400 mt-16 max-w-2xl"
+        className="prose prose-h2:text-yellow-300 prose-h3:text-gray-300 prose-h4:text-gray-300 prose-h4:ml-4 prose-h5:text-gray-500 prose-p:text-gray-200 prose-ul:text-gray-200 prose-pre:border-2 prose-pre:border-gray-400 prose-code:bg-gray-800 prose-code:text-white prose-a:text-cyan-300 prose-a:no-underline mt-16 max-w-2xl"
         dangerouslySetInnerHTML={{ __html: marked(content) }}
       />
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,10 @@ type HomeProps = {
   posts: Post[];
 };
 
+// https://howdy.com/
+// https://gumroad.com/
+// https://salehmubashar.com/
+
 export default function Home({ posts }: HomeProps) {
   return (
     <div className="relative">
@@ -18,13 +22,13 @@ export default function Home({ posts }: HomeProps) {
         <section className="flex flex-col border-b-2 border-white md:flex-row">
           <div className="flex-1 bg-pink-300 border-b-2 md:border-b-0 md:border-r-2 border-white p-[6.5vw]">
             <div className="max-w-xl mx-auto">
-              <h1 className="font-bold text-gray-100 text-6xl lg:text-[5.625rem] stroke-black">
-                Welcome
+              <h1 className="font-bold text-gray-100 text-6xl stroke-black">
+                Howdy, I'm Andrew!
               </h1>
-              <p className="text-gray-900 text-xl mt-12">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-                libero rerum, magnam quasi autem aliquam dolorem temporibus
-                consectetur maiores vel.
+              <p className="text-black text-xl mt-12">
+                I'm a Community taught Full-stack Mobile and Web Developer. This
+                site is to share what I am learning and hopefully help others
+                along the way.
               </p>
 
               <PrimaryButton href="/blog" className="mt-12" text="Read More" />
@@ -33,9 +37,16 @@ export default function Home({ posts }: HomeProps) {
           <div className="flex-1 bg-yellow-300 p-16 flex justify-center items-center">
             <div className="flex flex-col justify-center items-center">
               <Blocks />
-              <p className="text-gray-900 text-xl mt-8">
-                Some text about stuff
+              <p className="text-black text-xl mt-8">
+                Here's some stuff I enjoy learning about:
               </p>
+              <ul className="text-black text-xl mt-4 list-disc">
+                <li>JavaScript</li>
+                <li>Swift</li>
+                <li>CSS</li>
+                <li>React</li>
+                <li>React Native</li>
+              </ul>
             </div>
           </div>
         </section>
