b2fcd648ce167034dfc82962ff2a863dffbb0218
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-01-15 20:04:01 -0600

    refactor: back button moved into own component

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,12 +1,12 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
-import Link from "next/link";
 import { marked } from "marked";
 import { useEffect, useState } from "react";
 // import BlogOutlineCard from "@/components/BlogOutlineCard";
 import { HeadingElement } from "../../types";
 import ColorDivider from "@/components/ColorDivider";
+import BackButton from "@/components/BackButton";
 
 const PostPage = ({ frontmatter: { title, excerpt, date }, slug, content }) => {
   const [headingElements, setHeadingElements] = useState<HeadingElement[]>([]);
@@ -37,30 +37,7 @@ const PostPage = ({ frontmatter: { title, excerpt, date }, slug, content }) => {
   return (
     <div className="px-[10%] md:p-0 md:w-[40em] mx-auto mt-12">
       <div className="">
-        <Link
-          className="group flex items-center text-gray-400 hover:text-gray-200 w-fit transition-colors"
-          href="/blog"
-        >
-          <svg
-            className="transform rotate-180 mr-1 stroke-2 stroke-current fill-none"
-            width="10"
-            height="10"
-            viewBox="0 0 10 10"
-            aria-hidden="true"
-          >
-            <g fillRule="evenodd">
-              <path
-                className="transition opacity-0 group-hover:opacity-100"
-                d="M0 5h7"
-              ></path>
-              <path
-                className="transition group-hover:translate-x-[3px]"
-                d="M1 1l4 4-4 4"
-              ></path>
-            </g>
-          </svg>
-          <div className="ml-1">Go Back</div>
-        </Link>
+        <BackButton />
         <h1 className="text-4xl md:text-5xl lg:text-7xl mt-8">{title}</h1>
         <div className="text-gray-200 text-xl mt-2">{excerpt}</div>
         <div className="text-sm text-gray-400 mt-2">
