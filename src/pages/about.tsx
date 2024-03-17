1993d44e38c78075e939c403e003881e33258968
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-03-16 19:46:17 -0500

    feat: adds maps component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,83 +1,10 @@
 import { socialLinks } from "@/lib/utils/social";
-import * as d3 from "d3";
 import { useEffect } from "react";
-import us from "@/lib/data/us.json";
-import * as topojson from "topojson-client";
-
-type CreateMapParams = {
-  width: number;
-  height: number;
-  data: any;
-};
+import { ClientMap } from "@/components/d3/Map";
 
 const About = () => {
-  const createMap = ({ width, height, data }: CreateMapParams) => {
-    const path = d3.geoPath();
-    console.log("path: ", path);
-
-    const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
-
-    const svg = d3
-      .create("svg")
-      .attr("viewBox", [0, 0, width, height])
-      .attr("width", width)
-      .attr("height", height)
-      .attr("style", "max-width: 100%; height: auto;")
-      .on("click", reset);
-
-    const g = svg.append("g");
-
-    const states = g
-      .append("g")
-      .attr("fill", "#444")
-      .attr("cursor", "pointer")
-      .selectAll("path")
-      .data(topojson.feature(data, data.objects.states).features)
-      .join("path")
-      .on("click", clicked)
-      .attr("d", path);
-
-    function reset() {
-      states.transition().style("fill", null);
-      svg
-        .transition()
-        .duration(750)
-        .call(
-          zoom.transform,
-          d3.zoomIdentity,
-          d3.zoomTransform(svg.node()).invert([width / 2, height / 2])
-        );
-    }
-
-    function clicked(event, d) {
-      const [[x0, y0], [x1, y1]] = path.bounds(d);
-      event.stopPropagation();
-      states.transition().style("fill", null);
-      d3.select(this).transition().style("fill", "red");
-      svg
-        .transition()
-        .duration(750)
-        .call(
-          zoom.transform,
-          d3.zoomIdentity
-            .translate(width / 2, height / 2)
-            .scale(
-              Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height))
-            )
-            .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-          d3.pointer(event, svg.node())
-        );
-    }
-
-    function zoomed(event) {
-      const { transform } = event;
-      g.attr("transform", transform);
-      g.attr("stroke-width", 1 / transform.k);
-    }
-  };
-
   useEffect(() => {
-    createMap({ width: 600, height: 600, data: us });
+    // createMap({ width: 600, height: 600, data: us });
   }, []);
 
   return (
@@ -94,7 +21,7 @@ const About = () => {
         technologies. In my spare time, I enjoy learning about fitness and
         nutrition, woodworking and am a music aficionado.
       </p>
-
+      <ClientMap width={350} height={620} />
       <div className="flex flex-col items-start mt-10">
         {socialLinks.map(({ title, href }, index) => (
           <a key={index} className={`group ${index ? "mt-6" : ""}`} href={href}>
