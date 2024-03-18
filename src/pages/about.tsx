899c638c2bcdaf0101d96a877e354bfad781ae5a
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-03-18 12:31:15 -0500

    feat: adds bento layout section in aboutpage

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,20 +8,31 @@ const About = () => {
         About
       </h1>
 
-      <p className="text-xl mx-auto mt-10">
-        Hi, I&apos;m Andrew. I&apos;m a mobile developer living Bloomington,
-        Illinois. I am passionate about constantly learning and using new mobile
-        and web technologies. I currently work mainly with React Native and
-        Swift to build mobile applications, but always enjoy implementing new
-        technologies. In my spare time, I enjoy learning about fitness and
-        nutrition, woodworking and am a music aficionado.
-      </p>
-      <ClientMap width={350} height={620} />
+      <section className="mt-10">
+        <div className="grid grid-cols-3 grid-rows-3 bg-emerald-300 border-2 border-white rounded gap-4 p-4">
+          <div className="bg-black border-2 border-white rounded col-span-1 row-span-2"></div>
+          <div className="bg-black border-2 border-white rounded col-span-2 row-span-2 p-4">
+            <p className="text-lg">
+              I&apos;m Andrew. I&apos;m a mobile developer living Bloomington,
+              Illinois. I am passionate about constantly learning and using new
+              mobile and web technologies. I currently work mainly with React
+              Native and Swift to build mobile applications, but always enjoy
+              implementing new technologies. In my spare time, I enjoy learning
+              about fitness and nutrition, woodworking and am a music
+              aficionado.
+            </p>
+          </div>
+          <div className="bg-black border-2 border-white rounded col-span-1 row-span-1"></div>
+          <div className="bg-black border-2 border-white rounded col-span-1 row-span-1"></div>
+          <div className="bg-black border-2 border-white rounded col-span-1 row-span-1"></div>
+        </div>
+      </section>
+
       <div className="flex flex-col items-start mt-10">
         {socialLinks.map(({ title, href }, index) => (
           <a key={index} className={`group ${index ? "mt-6" : ""}`} href={href}>
             <div className="font-semibold text-cyan-300 text-xl">{title}</div>
-            <div className="bg-cyan-300 h-0.5 w-[0%] group-hover:w-full transition-all" />
+            <div className="bg-cyan-300 h-0.5 w-[0%] rounded-full group-hover:w-full transition-all" />
           </a>
         ))}
       </div>
