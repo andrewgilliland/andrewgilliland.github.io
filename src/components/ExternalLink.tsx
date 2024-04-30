97258a17bc9268a11af0dc6c22d4c8e6834c526a
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-04-30 17:02:23 -0500

    feat: updates about bentogrid

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,8 +1,8 @@
 const ExternalLink = ({ href, text }) => {
   return (
     <a className="group" href={href}>
-      <div className="font-semibold text-cyan-300 text-xl">{text}</div>
-      <div className="bg-cyan-300 h-0.5 w-[0%] rounded-full group-hover:w-full transition-all" />
+      <div className="font-semibold text-black text-xl">{text}</div>
+      <div className="bg-black h-0.5 w-[0%] rounded-full group-hover:w-full transition-all" />
     </a>
   );
 };
