22a340237f3ab478022482c5d7ac3199ea1bdbec
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-05-21 08:08:11 -0500

    feat: updates phone component

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -8,13 +8,14 @@ import {
 
 const Phone = () => {
   return (
-    <div className="relative border-2 border-black bg-gradient-to-b from-blue-500 to-gray-200 w-[64px] h-[132px] rounded-[12px] opacity-100 top-0 transition-all">
-      {/* <div className="absolute w-[64px] h-[132px] bg-black rounded-[12px]" /> */}
-      <div className="absolute left-5 top-0.5 bg-black w-5 h-1.5 rounded-full">
+    <div className="relative border-2 border-black bg-gray-700 w-[96px] h-[198px] rounded-[12px] opacity-100 transition-all">
+      <div className="absolute  bg-black w-[96px] h-[198px] rounded-[12px] translate-x-0.5 translate-y-0.5" />
+
+      <div className="absolute z-20 left-[36%] top-0.5 bg-black w-7 h-2 rounded-full">
         <div className="absolute left-[75%] top-[33%] bg-white w-0.5 h-0.5 rounded-full" />
       </div>
 
-      <div className="absolute z-10 bg-gradient-to-b from-gray-900 to-gray-700 w-full h-full rounded-[10px] opacity-100 hover:opacity-0 transition duration-500" />
+      <div className="absolute z-10 bg-gray-800 w-full h-full rounded-[10px] opacity-100 hover:opacity-0 transition duration-500" />
 
       <div className="absolute left-[14%] top-[10%] flex flex-col items-center font-sans">
         <div className="text-[4px]">{`${currentDayOfWeek}, ${currentMonthName} ${
@@ -22,10 +23,9 @@ const Phone = () => {
         }`}</div>
         <div className="text-[20px] leading-[20px]">{`${currentTime}:${currentMinutes}`}</div>
       </div>
-      <div className="absolute left-[-4%] top-[18.5%] bg-black h-[4%] w-[1%]" />
-      <div className="absolute left-[-4%] top-[26%] bg-black h-[7.5%] w-[1%]" />
-      <div className="absolute left-[-4%] top-[36%] bg-black h-[7.5%] w-[1%]" />
-      <div className="absolute right-[-4.5%] top-[30%] bg-black h-[12%] w-[1%]" />
+      <div className="absolute left-[-4%] top-[18.5%] bg-black h-[4%] w-[2%]" />
+      <div className="absolute left-[-4%] top-[26%] bg-black h-[7.5%] w-[2%]" />
+      <div className="absolute left-[-4%] top-[36%] bg-black h-[7.5%] w-[2%]" />
     </div>
   );
 };
--- src/components/pages/home/HomeBento.tsx
+++ src/components/pages/home/HomeBento.tsx
@@ -1,5 +1,6 @@
 import Block from "@/components/Block";
 import BrowserWindow from "@/components/BrowserWindow";
+import Phone from "@/components/Phone";
 import SvgIcon from "@/components/svg/SvgIcon";
 
 const HomeBento = () => {
@@ -17,14 +18,16 @@ const HomeBento = () => {
           />
         </div>
         <BrowserWindow />
+
+        <Phone />
         <div className="grid gap-5">
-          <Block
-            color="cyan"
-            Icon={<SvgIcon name="react" className="fill-blue-500" />}
-          />
           <Block
             color="emerald"
             Icon={<SvgIcon name="node" className="fill-emerald-500" />}
+          />
+          <Block
+            color="cyan"
+            Icon={<SvgIcon name="react" className="fill-blue-500" />}
           />
         </div>
       </div>
