9be1f343f49635d6fa0a022bd15b1ae287b724a8
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-05-21 17:07:07 -0500

    feat: updates block, svgicon, and homebento

--- src/components/pages/home/HomeBento.tsx
+++ src/components/pages/home/HomeBento.tsx
@@ -8,27 +8,26 @@ const HomeBento = () => {
     <div className="flex-1 bg-emerald-300 p-16 flex justify-center items-center">
       <div className="grid place-items-center grid-cols-2 gap-5">
         <div className="grid grid-cols-2 gap-5">
-          <Block
-            color="yellow"
-            Icon={<SvgIcon name="javascript" className="fill-yellow-500" />}
-          />
-          <Block
-            color="cyan"
-            Icon={<SvgIcon name="css" className="fill-cyan-500" />}
-          />
+          <Block backgroundColor="bg-yellow-300">
+            <SvgIcon name="javascript" className="fill-black" />
+          </Block>
+          <Block backgroundColor="bg-blue-200">
+            <SvgIcon name="css" className="fill-blue-600" />
+          </Block>
         </div>
         <BrowserWindow />
 
         <Phone />
-        <div className="grid  gap-5">
-          <Block
-            color="emerald"
-            Icon={<SvgIcon name="node" className="fill-emerald-500" />}
-          />
-          <Block
-            color="cyan"
-            Icon={<SvgIcon name="react" className="fill-blue-500" />}
-          />
+        <div className="grid gap-5">
+          <Block backgroundColor="bg-emerald-100">
+            <SvgIcon name="node" className="fill-emerald-600" />
+          </Block>
+          <Block backgroundColor="bg-gray-600">
+            <SvgIcon
+              name="react"
+              className="fill-sky-400 rotate-0 transition hover:rotate-180 duration-500 ease-in-out"
+            />
+          </Block>
         </div>
       </div>
     </div>
