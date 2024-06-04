f2013f6b482f1584618e014f84a6a692eaf6a8d3
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-06-04 16:42:42 -0500

    feat: updates active state for DownloadButton again

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -12,9 +12,9 @@ const DownloadButton = ({ className }: DownloadButtonProps) => {
       className={`group relative ${className}`}
       //   onClick={downloadPdf}
     >
-      <div className="absolute bg-pink-600 border-2 border-white w-full h-full rounded bottom-0 transform-gpu transition translate-x-2 translate-y-2 group-active:translate-x-0 group-active:translate-y-0" />
+      <div className="absolute bg-pink-600 border-2 border-white w-full h-full rounded bottom-0 transform-gpu transition group-hover:translate-x-2 group-hover:translate-y-2 group-active:translate-x-0 group-active:translate-y-0" />
       <div className="absolute bg-cyan-600 border-2 border-white w-full h-full rounded bottom-0" />
-      <div className="min-w-max md:text-2xl bg-black border-2 border-white rounded px-12 md:px-24 py-4 transform-gpu transition-all -translate-x-2 -translate-y-2 group-active:translate-x-0 group-active:translate-y-0 group-hover:bg-emerald-900">
+      <div className="min-w-max md:text-2xl bg-black border-2 border-white rounded px-12 md:px-24 py-4 transform-gpu transition group-hover:-translate-x-2 group-hover:-translate-y-2 group-active:translate-x-0 group-active:translate-y-0">
         Download
       </div>
     </button>
