b38420ae0e26209398af47fe7fa297b87a0aaed1
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-07-08 22:08:08 -0500

    feat: adds colors to socialLinks

--- src/lib/utils/social.ts
+++ src/lib/utils/social.ts
@@ -7,15 +7,18 @@ export const socialLinks = [
     title: "GitHub",
     Icon: GitHubIcon,
     href: "https://github.com/andrewgilliland",
+    color: "cyan",
   },
   {
     title: "LinkedIn",
     Icon: LinkedInIcon,
     href: "https://www.linkedin.com/in/andrew-gilliland-a89b8919a/",
+    color: "pink",
   },
   {
     title: "Twitter",
     Icon: TwitterIcon,
     href: "https://twitter.com/droidgilliland",
+    color: "yellow",
   },
 ];
