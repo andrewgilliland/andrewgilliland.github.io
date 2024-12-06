14dc33ae6a9744e98d93314dae9cedbecc135c8d
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-12-06 12:54:48 -0600

    feat: updates AboutPage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,31 +4,23 @@ import Link from "next/link";
 
 const AboutPage = () => {
   return (
-    <div className="max-w-3xl w-full mx-auto px-8 py-14 bg-emerald-300">
-      <h1 className="font-bold text-white text-3xl md:text-4xl stroke-black md:mx-0">
+    <div className="mx-auto w-full max-w-3xl bg-emerald-300 px-8 py-14">
+      <h1 className="stroke-black text-3xl font-bold text-white md:mx-0 md:text-4xl">
         About
       </h1>
 
-      <section className="mt-10 grid gap-4 sm:grid-cols-2 sm:gap-10">
-        <div className="grid bg-cyan-300 rounded-lg overflow-hidden border-2 border-black p-16 sm:px-16">
-          <Image
-            src="/andy-config-2.jpg"
-            width={800}
-            height={500}
-            alt="Andrew Gilliland"
-            className="rounded-lg border-2 border-black self-center"
-          />
-        </div>
-        <div className="grid gap-4 justify-items-center font-semibold text-black text-base md:text-lg bg-yellow-300 border-2 border-black rounded-lg p-4">
-          <p>
-            Hola, I&apos;m Andrew. I am a full stack developer with domain
-            expertise in mobile development. I am proficient working with React
-            Native and SwiftUI to build mobile applications, but am always
-            learning and implementing new technologies. In my spare time, I
-            enjoy learning about fitness, nutrition, woodworking, venturing into
-            nature, and going to concerts.
-          </p>
-          <Link href="/resume">
+      <section className="mt-10 grid grid-rows-3 gap-4 sm:grid-cols-2 sm:gap-10">
+        <div className="grid justify-items-center overflow-hidden rounded-lg border-2 border-black bg-cyan-300 px-16 py-4">
+          <div className="relative overflow-hidden">
+            <Image
+              src="/andy-config-2.jpg"
+              width={800}
+              height={500}
+              alt="Andrew Gilliland"
+              className="h-40 w-40 rounded-lg border-2 border-black object-cover object-left-top"
+            />
+          </div>
+          <Link className="mt-4" href="/resume">
             <Block
               backgroundColor="bg-pink-500"
               height="12"
@@ -38,6 +30,37 @@ const AboutPage = () => {
               Resume
             </Block>
           </Link>
+        </div>
+        <div className="grid justify-items-center gap-4 rounded-lg border-2 border-black bg-yellow-300 p-4 text-base font-semibold text-black">
+          <p>
+            I&apos;m a self-taught full stack developer who loves building tools
+            to solve business problems. I believe development is a team sport
+            and thrive in collaborative environments that encourage creative
+            growth—after all, the best ideas can come from anyone. My mission is
+            to help teams create exceptional web and mobile applications that
+            reach their full potential.
+          </p>
+        </div>
+        <div className="grid justify-items-center gap-4 rounded-lg border-2 border-black bg-yellow-300 p-4 text-base font-semibold text-black">
+          <p>
+            I thrive on building engaging and interactive user experiences and
+            am comfortable working in both front-end and back-end development.
+            For mobile applications, I prefer working with TypeScript, React
+            Native, and Expo. For web applications, my go-to tools are
+            TypeScript, React, and Next.js. I&apos;m always eager to learn and
+            work with new technologies.
+          </p>
+        </div>
+        <div className="grid justify-items-center gap-4 rounded-lg border-2 border-black bg-yellow-300 p-4 text-base font-semibold text-black">
+          <p>
+            Outside of programming, I love fitness, exercise, and nutrition.
+            Having worked as a personal trainer for several years, I thoroughly
+            enjoy sharing my knowledge with others. My other interests include
+            woodworking, reading, drawing, grilling, and tackling various home
+            projects. When it&apos;s time to relax, you&apos;ll find me hanging
+            out with my dog Hank, my cats Gus and Bernie, while enjoying some
+            Yacht Rock.
+          </p>
         </div>
       </section>
     </div>
