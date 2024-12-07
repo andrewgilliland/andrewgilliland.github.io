7bf0dd75efb321c610a20dfff53e6ddb67766b26
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-12-07 06:38:16 -0600

    feat: updates GridPattern

--- src/components/patterns/GridPattern.tsx
+++ src/components/patterns/GridPattern.tsx
@@ -2,22 +2,17 @@ import {
   DevicePhoneMobileIcon,
   DeviceTabletIcon,
   ComputerDesktopIcon,
+  CircleStackIcon,
+  CodeBracketSquareIcon,
 } from "@heroicons/react/24/outline";
 
 const GridPattern = () => {
   const icons = [
-    <ComputerDesktopIcon
-      key="desktop"
-      className="h-8 w-8 rotate-6 text-emerald-200"
-    />,
-    <DevicePhoneMobileIcon
-      key="phone"
-      className="h-8 w-8 rotate-12 text-emerald-200"
-    />,
-    <DeviceTabletIcon
-      key="tablet"
-      className="h-8 w-8 -rotate-45 text-emerald-200"
-    />,
+    <ComputerDesktopIcon key="desktop" className="h-8 w-8 rotate-6" />,
+    <DevicePhoneMobileIcon key="phone" className="h-8 w-8 rotate-12" />,
+    <DeviceTabletIcon key="tablet" className="h-8 w-8 -rotate-45" />,
+    <CircleStackIcon key="circle" className="h-8 w-8 -rotate-12" />,
+    <CodeBracketSquareIcon key="code" className="h-8 w-8 rotate-45" />,
   ];
   const duplicatedIcons: JSX.Element[] = [];
   for (let i = 0; i < 24; i++) {
@@ -25,7 +20,7 @@ const GridPattern = () => {
   }
 
   return (
-    <div className="absolute grid w-full grid-cols-4 place-items-center gap-20">
+    <div className="absolute grid w-full grid-cols-4 place-items-center gap-20 text-emerald-200">
       {duplicatedIcons.map((icon, index) => (
         <div key={index}>{icon}</div>
       ))}
