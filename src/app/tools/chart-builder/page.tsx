956363d81801a7b4a871773936a1e0226e6e8609
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-12-12 16:59:02 -0600

    feat: adds download button to ChartBuilderPage

--- src/app/tools/chart-builder/page.tsx
+++ src/app/tools/chart-builder/page.tsx
@@ -1,11 +1,17 @@
 import PageLayout from "@/components/layouts/PageLayout";
-import PageHeading from "@/components/typography/PageHeading";
+import { ArrowDownOnSquareIcon } from "@heroicons/react/24/outline";
 
 const ChartBuilderPage = () => {
   return (
     <PageLayout title="Chart Builder">
       <section className="mx-8 mb-24 mt-10 md:mx-0 md:mt-0">
-        stuff to come
+        <a
+          className="flex max-w-fit cursor-pointer items-center gap-2 rounded-lg border-2 border-white bg-pink-600 px-4 py-3"
+          download={""}
+        >
+          <span className="font-semibold text-white">Download</span>
+          <ArrowDownOnSquareIcon className="h-6 w-6 stroke-2 text-white" />
+        </a>
       </section>
     </PageLayout>
   );
