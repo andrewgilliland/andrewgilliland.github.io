5edcbdb84f9011a649ba3134cd6a43c59daf70d3
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-12-13 10:05:49 -0600

    feat: adds chart builder select

--- src/app/tools/chart-builder/page.tsx
+++ src/app/tools/chart-builder/page.tsx
@@ -1,12 +1,51 @@
+"use client";
+
 import PageLayout from "@/components/layouts/PageLayout";
+import { useEffect, useState } from "react";
 import { ArrowDownOnSquareIcon } from "@heroicons/react/24/outline";
 
+type ChartType = "area" | "bar" | "line" | "pie";
+
 const ChartBuilderPage = () => {
+  const [chartType, setChartType] = useState<ChartType>("line");
+
+  const handleChartTypeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    setChartType(event.target.value as ChartType);
+  };
+
+  useEffect(() => {
+    console.log(chartType);
+  }, [chartType]);
+
   return (
     <PageLayout title="Chart Builder">
       <section className="mx-8 mb-24 mt-10 md:mx-0 md:mt-0">
+        <div>
+          <label
+            htmlFor="chartType"
+            className="mb-2 block text-sm font-medium text-white"
+          >
+            Select Chart Type
+          </label>
+          <select
+            id="chartType"
+            value={chartType}
+            onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
+              setChartType(event.target.value as ChartType)
+            }
+            className="rounded-lg border-2 border-white bg-black px-4 py-3"
+          >
+            <option value="line">Line Chart</option>
+            <option value="bar">Bar Chart</option>
+            <option value="area">Area Chart</option>
+            <option value="pie">Pie Chart</option>
+          </select>
+        </div>
+
         <a
-          className="flex max-w-fit cursor-pointer items-center gap-2 rounded-lg border-2 border-white bg-pink-600 px-4 py-3"
+          className="mt-4 flex max-w-fit cursor-pointer items-center gap-2 rounded-lg border-2 border-white bg-pink-600 px-4 py-3"
           download={""}
         >
           <span className="font-semibold text-white">Download</span>
