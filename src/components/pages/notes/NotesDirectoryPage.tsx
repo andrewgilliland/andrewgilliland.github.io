378f53804867035ac6555381af0056bb91d50eed
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-12-20 15:34:24 -0600

    feat: wip more updates to NotesPage ui

--- src/components/pages/notes/NotesDirectoryPage.tsx
+++ src/components/pages/notes/NotesDirectoryPage.tsx
@@ -1,8 +1,6 @@
 import { FC } from "react";
 import { getNoteDirectory } from "@/lib/actions/notes";
-import TopicCard from "@/components/TopicCard";
-import NoteCard from "@/components/NoteCard";
-import CardGrid from "@/components/CardGrid";
+
 import { FolderPlusIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
@@ -32,66 +30,55 @@ const NotesDirectoryPage: FC<NotesDirectoryPageProps> = async ({
             will add resources to give due credit where info comes from. Click
             on a topic below to view notes related to that topic.
           </p>
-          {/* {topics.length > 0 && (
-          <CardGrid>
-          {topics.map((topic, index) => (
-            <TopicCard key={index} topic={topic} />
-            ))}
-            </CardGrid>
-            )} */}
         </section>
       )}
 
       <section className="mx-8 mb-24 mt-10 md:mx-0 md:mt-0">
         {notes.length > 0 && (
-          <div className="grid overflow-hidden rounded-lg border-2 border-white">
-            <div className="bg-cyan-300 px-4 py-2">
-              <h2 className="text-lg font-bold text-black">
-                {directory ? decodeURIComponent(directory) : "Notes"}
-              </h2>
+          <div className="rounded border-2 border-white">
+            <div className="grid overflow-hidden rounded border-2 border-black">
+              <div className="rounded-t bg-cyan-300 px-4 py-2">
+                <h2 className="text-lg font-bold text-black">
+                  {directory ? decodeURIComponent(directory) : "Notes"}
+                </h2>
+              </div>
+              {topics.map((topic, index) => (
+                <Link
+                  href={`/${topic.path.toLocaleLowerCase()}`}
+                  key={index}
+                  className={`flex items-center gap-2 px-4 py-3 transition-colors hover:bg-gray-900 ${index && "border-t border-gray-700"}`}
+                >
+                  <FolderPlusIcon
+                    className={`h-6 w-6 fill-black stroke-white`}
+                  />
+                  <span className="text-base font-bold text-white">
+                    {topic.name}
+                  </span>
+                </Link>
+              ))}
+              {notes.map(({ path, frontmatter }, index) => (
+                <Link
+                  href={`/notes/${path!}`}
+                  key={index}
+                  className={`transition-colors hover:bg-gray-900 ${true && "border-t border-gray-700"}`}
+                  title={frontmatter!.title}
+                >
+                  <div className="flex w-3/4 items-center gap-2 px-6 py-3">
+                    <div>
+                      <PencilSquareIcon
+                        className={`h-5 w-5 fill-black stroke-white`}
+                      />
+                    </div>
+                    <p className="truncate text-base font-bold text-white">
+                      {frontmatter!.title}
+                    </p>
+                  </div>
+                </Link>
+              ))}
             </div>
-            {topics.map((topic, index) => (
-              <Link
-                href={`/${topic.path.toLocaleLowerCase()}`}
-                key={index}
-                className={`flex items-center gap-2 px-4 py-2 ${index && "border-t"}`}
-              >
-                <FolderPlusIcon className={`h-6 w-6 fill-black stroke-white`} />
-                <span className="text-base font-bold text-white">
-                  {topic.name}
-                </span>
-              </Link>
-            ))}
-            {notes.map((note, index) => (
-              <Link
-                href={`/notes/${note.path!}`}
-                key={index}
-                className={`flex items-center gap-2 px-6 py-2 ${true && "border-t"}`}
-              >
-                <PencilSquareIcon
-                  className={`h-5 w-5 fill-black stroke-white`}
-                />
-                <span className="text-base font-bold text-white">
-                  {note.frontmatter.title}
-                </span>
-              </Link>
-            ))}
           </div>
         )}
       </section>
-
-      {/* {notes.length > 0 && (
-        <section className="mx-8 mb-24 mt-10 md:mx-0">
-          <h2 className="stroke-white text-3xl font-bold capitalize text-gray-100 md:text-4xl">
-            {decodeURIComponent(directory)}
-          </h2>
-          <CardGrid>
-            {notes.map((note, index) => (
-              <NoteCard key={index} note={note} index={index} />
-            ))}
-          </CardGrid>
-        </section>
-      )} */}
     </div>
   );
 };
