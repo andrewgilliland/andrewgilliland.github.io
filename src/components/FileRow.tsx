219cd9116d3a4a741ca3dfe79a44bec96aacc8c5
Andrew Gilliland <andrewpgilliland@gmail.com>
2024-12-26 22:36:54 -0600

    feat: updates FileRow width

--- src/components/FileRow.tsx
+++ src/components/FileRow.tsx
@@ -14,7 +14,7 @@ const FileRow: FC<FileRowProps> = ({ fileNode }) => {
       title={fileNode.title}
       className="flex border-b border-gray-700 px-4 py-3 transition-colors hover:bg-gray-900"
     >
-      <div className="flex w-3/4 items-center gap-2">
+      <div className="flex w-full items-center gap-2">
         <div>
           <PencilSquareIcon className="h-5 w-5 stroke-2 text-pink-400" />
         </div>
