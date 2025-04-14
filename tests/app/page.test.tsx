6f667a04e7656bc4bb8de54d9c1d5c304d5ab833
Andrew Gilliland <agilliland@goodjava.org>
2025-04-14 11:05:12 -0500

    feat: adds husky precommit

--- tests/app/page.test.tsx
+++ tests/app/page.test.tsx
@@ -1,13 +1,12 @@
 import { expect } from "@jest/globals";
 import "@testing-library/jest-dom";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import HomePage from "@/app/page";
 
 describe("HomePage", () => {
   it("should render learn react link", async () => {
     // Arrange
     render(<HomePage />);
-
     // Act
     const linkElement = screen.findByText(/learn react/i);
     // Assert
